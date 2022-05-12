219a905121cf1a0df2b0220727e2e6126b5ffbda
herr-code <herrwebj@gmail.com>
2022-05-11 20:47:25 -0500

    Creacion de la clase Reader.js y una prueba extra

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,8 +1,16 @@
+const Reader = require("./../../lib/utils/Reader");
+
 describe("Tests for ./lib/utils/Reader.js", () => {
     test("The response when reading the file cannot be null, undefined or empty (data.json)", () => {
         const result = Reader.readJsonFile("./test/utils/data/data.json");
         expect(result).not.toBeNull();
         expect(result).not.toBeUndefined();
         expect(result.length).toBeGreaterThan(0);
+    });
+
+    test("It must print the correct data[0]", () => {
+        const result = Reader.readJsonFile("./test/utils/data/data.json");
+        expect(result[0].name).toBe("Leanne Graham");
+        expect(result[0].username).toBe("Bret");
     });
 });
