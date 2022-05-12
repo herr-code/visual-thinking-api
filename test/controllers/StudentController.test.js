a970fa53e33aa5a0245e8a4bf89a2b927cb6c567
herr-code <herrwebj@gmail.com>
2022-05-12 00:41:21 -0500

    Codigo para pasar la prueba StudentController.getListOfStudents()

--- test/controllers/StudentController.test.js
+++ test/controllers/StudentController.test.js
@@ -1,7 +1,14 @@
+const StudentController = require("./../../lib/controllers/StudentController");
+
 describe("Tests for StudentController", () => {
     test("Requirement 1: Get all students using StudentService", () => {
-        const listOfStudents = ExplorerController.getListOfStudents();
+        const listOfStudents = StudentController.getListOfStudents();
         expect(typeof listOfStudents).toBe("object");
-        expect(listOfStudents.length).toBe(10);
+        expect(listOfStudents.length).toBe(51);
+        expect(listOfStudents[0].name).toBe("Warren");
+        expect(listOfStudents[0].email).toBe("Todd@visualpartnership.xyz");
+        expect(listOfStudents[0].credits).toBe(508);
+        expect(listOfStudents[0].previousCourses).toBe(1);
+        expect(listOfStudents[0].haveCertification).toBeTruthy();
     });
 });
