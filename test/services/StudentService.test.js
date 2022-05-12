96cae09e018eed6027644d1237fbe0a6f14ff3c8
herr-code <herrwebj@gmail.com>
2022-05-12 13:26:31 -0500

    Prueba inicial de StudentService.getEmailStudentsWithCertification()

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -2,11 +2,11 @@ const StudentService = require("./../../lib/services/StudentService");
 
 describe("Tests for StudentService", () => {
     const students = [
-        {name: "Yoselin", email: "yos@gmail.com", credits: 350},
-        {name: "Teresa", email: "tere@gmail.com", credits: 400},
-        {name: "Diana", email: "diana@gmail.com", credits: 600},
-        {name: "Abril", email: "abril@gmail.com", credits: 650},
-        {name: "Fatima", email: "fatima@gmail.com", credits: 700},
+        {name: "Yoselin", email: "yos@gmail.com", credits: 350, haveCertification: true},
+        {name: "Teresa", email: "tere@gmail.com", credits: 400, haveCertification: false},
+        {name: "Diana", email: "diana@gmail.com", credits: 600, haveCertification: false},
+        {name: "Abril", email: "abril@gmail.com", credits: 650, haveCertification: false},
+        {name: "Fatima", email: "fatima@gmail.com", credits: 700, haveCertification: true}
     ];
     test("Requirement 1: Get all students", () => {
         const studentsList = StudentService.getAllStudents(students);
@@ -15,5 +15,14 @@ describe("Tests for StudentService", () => {
         expect(studentsList[4].name).toBe("Fatima");
         expect(studentsList[4].email).toBe("fatima@gmail.com");
         expect(studentsList[4].credits).toBe(700);
+    });
+
+    test("Requirement 2: Get all email of students with certification", () => {
+        const studentsListEmail = StudentService.getEmailStudentsWithCertification(students);
+        expect(typeof studentsList).toBe("object");
+        expect(studentsList.length).toBe(2);
+        expect(studentsListEmail[0].name).toBe("Yoselin");
+        expect(studentsListEmail[0].email).toBe("yos@gmail.com");
+        expect(studentsListEmail[0].credits).toBe(350);
     });
 });
